df4bfb2456ec900f85390b28c5de0965c972453c
Douglascrc <douglasbuscam@gmail.com>
2025-12-16 09:53:36 -0300

    first commit

---
+++ src/main/java/com/hackathon/sentiment/api/SentimentAnalysisApiApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.sentiment.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentAnalysisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentimentAnalysisApiApplication.class, args);
+	}
+
+}
